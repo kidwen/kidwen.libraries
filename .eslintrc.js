38f488eafbc351fb2f8bd6876ab22e08c4b26d62
kidwen <xuqiujia2020@outlook.com>
2021-06-20 19:26:09 +0800

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,9 +15,41 @@ module.exports = {
     },
     ignorePatterns: ['.eslintrc.js'],
     rules: {
+        '@typescript-eslint/adjacent-overload-signatures': 'error',
+        '@typescript-eslint/array-type': ['error', { 'default': 'generic' }],
+        '@typescript-eslint/await-thenable': 'error',
+        "@typescript-eslint/ban-ts-comment": "error",
+        "@typescript-eslint/ban-types": [
+            "error",
+            {
+                "types": {
+                    "Object": {
+                        "message": "Use {} instead."
+                    },
+                    "String": null
+                }
+            }
+        ],
         '@typescript-eslint/interface-name-prefix': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/consistent-type-assertions': 'error',
+        '@typescript-eslint/explicit-function-return-type': 'error',
+        '@typescript-eslint/explicit-member-accessibility': 'error',
+        '@typescript-eslint/member-delimiter-style': 'error',
+        '@typescript-eslint/no-confusing-void-expression': 'error',
+        '@typescript-eslint/no-implicit-any-catch': 'error',
+        '@typescript-eslint/no-unsafe-call': 'error',
+        '@typescript-eslint/type-annotation-spacing': 'error',
+        '@typescript-eslint/typedef': ['error', {
+            "arrayDestructuring": true,
+            "arrowParameter": true,
+            "memberVariableDeclaration": true,
+            "objectDestructuring": true,
+            "parameter": true,
+            "propertyDeclaration": true,
+            "variableDeclaration": true,
+            "variableDeclarationIgnoreFunction": true
+        }],
+        'quotes': ["error", "single"]
     },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+async function bootstrap(): Promise<void> {
+    const app: INestApplication = await NestFactory.create(AppModule);
     await app.listen(3000);
 }
 bootstrap();
